dbe578968243d19e84125d9ccf019006aaa4b97c
Andrew Moll <mollandrew913@gmail.com>
2015-11-01 12:25:52 +0800

    Added door.ex and modified portal.ex

--- lib/portal.ex
+++ lib/portal.ex
@@ -1,19 +1,49 @@
 defmodule Portal do
-  use Application
+	use Application
+	
+	# See http://elixir-lang.org/docs/stable/elixir/Application.html
+	# for more information on OTP Applications
+	def start(_type, _args) do
+		import Supervisor.Spec, warn: false
+		
+		children = [
+		# Define workers and child supervisors to be supervised
+		# worker(Portal.Worker, [arg1, arg2, arg3]),
+		]
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
-  def start(_type, _args) do
-    import Supervisor.Spec, warn: false
+		# See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+		# for other strategies and supported options
+		opts = [strategy: :one_for_one, name: Portal.Supervisor]
+		Supervisor.start_link(children, opts)
+	end
+	
+	defstruct [:left, :right]
+	
+	@doc """
+	Starts transfering `data` from `left` to `right`
+	"""	
+	def transfer(left, right, data) do
+		# First add all data to the portal on the left
+		for item <- data do
+			Portal.Door.push(left, item)
+		end
+		
+		# Returns a portal struct we will use next
+		%Portal{left: left, right: right}
+	end
+	
+	@doc """
+	Pushes data to the right in the given `portal`
+	"""
+	def push_right(portal) do
+		# See if we can pop data from Left. If so, push the
+		# popped data tot the right. Otherwise, do nothing.
+		case Portal.Door.push(portal.right, h) do
+			:error	-> :ok
+			{:ok, h}	-> Portal.Door.push(portal.right, h)
+		end
+		
+		portal
+	end
 
-    children = [
-      # Define workers and child supervisors to be supervised
-      # worker(Portal.Worker, [arg1, arg2, arg3]),
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Portal.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
 end
