7a5ff2b51f68cf56014bc23fc11d80a55358b1a8
Andrew Moll <mollandrew913@gmail.com>
2016-09-13 23:14:40 +0800

    Added the push function and made an unquote fragment for the push_right and push_left functions

--- lib/portal.ex
+++ lib/portal.ex
@@ -34,34 +34,53 @@ defmodule Portal do
 	@doc """
 	Pushes data to the right in the given `portal`.
 	"""
-	def push_right(portal) do
-		# See if we can pop data from left. If so, push the
-		# popped data to the right. Otherwise, do nothing.
-		case Portal.Door.pop(portal.left) do
-			:error   -> :ok
-			{:ok, h} -> Portal.Door.push(portal.right, h)
-		end
-
-		# Let's return the portal itself
-		portal
-	end
+  [:left, :right] |> Enum.map(fn direction ->
+    def unquote(:"push_#{direction}")(portal) do
+      push(portal, unquote(direction))
+    end
+  end)
+  
+	def push(portal, direction) do
+    {entrance, exit} = get_entrance_exit(portal, direction)
+    case Portal.Door.pop(entrance) do
+      :error -> :ok
+      {:ok, h} -> Portal.Door.push(exit, h)
+    end
+    
+    portal
+  end
 	
-	def push_left(portal) do
-		# See if we can pop data from left. If so, push the
-		# popped data to the right. Otherwise, do nothing.
-		case Portal.Door.pop(portal.right) do
-			:error   -> :ok
-			{:ok, h} -> Portal.Door.push(portal.left, h)
-		end
-
-		# Let's return the portal itself
-		portal
-	end
-	
+  defp get_entrance_exit(portal, :left) do
+    {portal.right, portal.left}
+  end
+  
+  defp get_entrance_exit(portal, :right) do
+    {portal.left, portal.right}
+  end
+  
 	@doc """
 	Shoots a new door with the given `color`.
 	"""
 	def shoot(color) do
 		Supervisor.start_child(Portal.Supervisor, [color])
+	end
+end
+
+defimpl Inspect, for: Portal do
+	def inspect(%Portal{left: left, right: right}, _) do
+		left_door	= inspect(left)
+		right_door	= inspect(right)
+		
+		left_data	= inspect(Enum.reverse(Portal.Door.get(left)))
+		right_data	= inspect(Portal.Door.get(right))
+		
+		max = max(String.length(left_door), String.length(left_data))
+		
+		"""
+		#Portal<
+			#{String.rjust(left_door, max)} <=> #{right_door}
+			#{String.rjust(left_data, max)} <=> #{right_data}
+		>
+		"""
 	end
 end
--- test/portal_test.exs
+++ test/portal_test.exs
@@ -1,7 +1,7 @@
 defmodule PortalTest do
   use ExUnit.Case
   doctest Portal
-
+  
   test "the truth" do
     assert 1 + 1 == 2
   end
